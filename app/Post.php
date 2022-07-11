c6f4b5994cdb3480683aabcbf5b05d3a373e503b
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-07-11 16:55:47 +0200

    creata la relazione nei model

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Str;
 class Post extends Model
 {
 
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
+    }
+
     public function category(){
         return $this->belongsTo('App\Category');
     }
